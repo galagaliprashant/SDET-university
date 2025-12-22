c62bb2e873c919ac19887f17c3a331ff805aedbf
galagaliprashant <galagaliprashanth@gmail.com>
2025-12-22 12:41:13 +0530

    Day 4 and Day 5 changes pushed together

---
+++ day4_data_structures.py
@@ -0,0 +1,18 @@
+# my_test_ques = ["login","search","Checkout"]
+#
+# my_test_ques.append("Hello") #to add the new element
+# print(f"after the append fuction, the que looked like this {my_test_ques}")
+#
+# my_test_ques.remove("Hello") #to delete the element
+# print(f"after the remove fuction, the que looked like this {my_test_ques}")
+#
+# que_lenght = len(my_test_ques) #Calaulate the que length
+# print(f"after the finding the length fuction, the que looked like this {my_test_ques}")
+from day1_reverse import striped_ID
+
+#tasks to create a dictionary
+# Added quotes around "20/11/90"
+student_record = {"Name": "Prassh", "DOB": "20/11/90", "Interest": "Building"}
+
+print(f"After the definition: {student_record}")
+print(f"Student DOB: {student_record['DOB']}")`
